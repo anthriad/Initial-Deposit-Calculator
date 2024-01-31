4650ff5619fc74064246dc6dd1cb1d93d6bb7a47
Anthony Riad <131625149+anthriad@users.noreply.github.com>
2024-01-31 11:09:24 -0500

    Add files via upload

---
+++ InitialDepositAmtCalc.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class InitialDepositAmtCalc {
+  public static void main (String[] args){
+    Scanner input = new Scanner(System.in);
+     
+    // Prompt user to input final account value
+    System.out.print("Enter Final Account Value: ");
+    double finalAccountValue = input.nextDouble();
+
+    //Prompt user to input annual interest rate in percentage
+    System.out.print("Enter Annual Interest Rate Percentage:");
+    double annualInterestRate = input.nextDouble();
+
+    //Prompt user to input number of years
+    System.out.print("Enter number of years: ");
+    double years = input.nextDouble();
+
+    //Calculate initial deposit
+   double initialDeposit = finalAccountValue / Math.pow(1 + annualInterestRate / 100,years);
+
+    //Print initial deposit
+    System.out.println("Initial deposit value is :" + initialDeposit);
+  }
+}
